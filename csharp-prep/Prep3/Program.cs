4a7393473546f990de228d5749436d1e031d20af
definitely-a-human <mike.makes.robots.24123@gmail.com>
2023-01-11 23:02:24 -0700

    Fixed another issue

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,16 +7,15 @@ class Program
         Random random = new Random();
         int magicNumber = random.Next(1, 100);
 
-        Console.Write("Guess the MAGIC number between 1 and 100. ? ");
-        string guessInput = Console.ReadLine();
-        int guess = int.Parse(guessInput);
-
+        Console.Write("Guess the MAGIC number between 1 and 100.");
+        
+        int guess = -1;
         int count = 1;
 
         while (guess != magicNumber)
         {
             Console.Write("What is your guess? ");
-            guessInput = Console.ReadLine();
+            string guessInput = Console.ReadLine();
             guess = int.Parse(guessInput);
 
             Console.WriteLine(" ");
