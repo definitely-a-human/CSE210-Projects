ac54b6265c5f964f7608c6d3d41e995b5385eebb
definitely-a-human <mike.makes.robots.24123@gmail.com>
2023-01-11 22:31:33 -0700

    finished program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+        Console.Write("Select a MAGIC number. ");
+        string magicInput = Console.ReadLine();
+        int magicNumber = int.Parse(magicInput);
+
+        Console.WriteLine(" ");
+
+        int guess = 0;
+        int count = 1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+
+            Console.WriteLine(" ");
+
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower. ");
+
+                Console.WriteLine(" ");
+
+                count++;
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher. ");
+
+                Console.WriteLine(" ");
+
+                count++;
+            }
+            else
+            {
+                Console.Write("You guessed it! It took you " + count + " tries. ");
+            }
+        }
+        
+        
     }
 }
