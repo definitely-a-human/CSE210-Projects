e39c3aef0581f8c5c4d0787c4f63453ce19ec111
definitely-a-human <mike.makes.robots.24123@gmail.com>
2023-01-11 23:05:01 -0700

    writeline fixed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,14 +7,14 @@ class Program
         Random random = new Random();
         int magicNumber = random.Next(1, 100);
 
-        Console.Write("Guess the MAGIC number between 1 and 100.");
+        Console.WriteLine("Guess the MAGIC number between 1 and 100.");
         
         int guess = -1;
         int count = 1;
 
         while (guess != magicNumber)
         {
-            Console.Write("What is your guess? ");
+            Console.WriteLine("What is your guess? ");
             string guessInput = Console.ReadLine();
             guess = int.Parse(guessInput);
 
